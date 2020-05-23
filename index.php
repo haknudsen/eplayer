cb6f561a9e22aa7bf3ef2494a06ac0c52b2771b1
haknudsen <paris_giovanni@yahoo.com>
2020-05-23 04:26:28 -0600

    strating to work on sort module
    changing to elarning project

---
+++ index.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="robots" content="index, follow">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="language" content="English">
+    <meta name="revisit-after" content="30 days">
+    <meta name="author" content="WebsiteTalkingHeads.com">
+    <title>eLearning Videos from Talking Heads&reg;</title>
+    <meta name="title" content="eLearning Videos from Talking Heads">
+    <meta name="description" content="We can describe your product, service or procedure in an eLearning Video! Our eLearning Videos focus on your message.">
+    <meta name="keywords" content="eLearning Corporate Videos, eLearning Explainer Video, eLearning Explainer Videos, eLearning Whiteboard Videos, eLearning Video, eLearning">
+    <?php include("../../../includes/css-b4.php"); ?>
+    <link href="css/talkingheadsplayer.min.css" rel="stylesheet" type="text/css">
+</head>
+<?php
+$pageURL = 'http';
+if ( $_SERVER[ "HTTPS" ] == "on" ) {
+    $pageURL .= "s";
+}
+$pageURL .= "://";
+if ( $_SERVER[ "SERVER_PORT" ] != "80" ) {
+    $pageURL .= $_SERVER[ "SERVER_NAME" ] . ":" . $_SERVER[ "SERVER_PORT" ] . $_SERVER[ "REQUEST_URI" ];
+} else {
+    $pageURL .= $_SERVER[ "SERVER_NAME" ] . $_SERVER[ "REQUEST_URI" ];
+}
+if ( isset( $_SERVER[ 'HTTP_X_FORWARDED_FOR' ] ) && $_SERVER[ 'HTTP_X_FORWARTDED_FOR' ] != '' ) {
+    $sentIP = $_SERVER[ 'HTTP_X_FORWARDED_FOR' ];
+} else {
+    $sentIP = $_SERVER[ 'REMOTE_ADDR' ];
+}
+?>
+<body>
+<?php include("../../../includes/header_b4.php"); ?>
+<section class="h-100 w-100 py-2">
+    <div id="elearning-process" class="embed-responsive embed-responsive-16by9">
+        <div id="player-holder" class="embed-responsive-item">
+            <video autoplay playsinline preload id="talking-head-player" width="100%">
+                <preference name="AllowInlineMediaPlayback" value="true" />
+                <p>Your browser does not support the video tag. Please Visit
+                    <a href="https://www.websitetalkingheads.com/support/" title="Please Visit Support">
+                        https://www.websitetalkingheads.com/support/
+                    </a>
+                </p>
+            </video>
+            <div id="bigPlayBtn"></div>
+            <div id="controls">
+                <div id="btn-restart" class="player-btn btn-restart" title="replay" accesskey="R"></div>
+                <div id="btn-play-toggle" class="player-btn btn-play" title="play" accesskey="P"></div>
+                <div id="btn-stop" class="player-btn btn-stop" title="stop" accesskey="X"></div>
+                <input type="range" id="volume-bar" title="volume" min="0" max="1" step="0.1" value="1">
+                <div id="btn-mute" class="player-btn btn-unmute" title="mute"></div>
+                <div class="progress" id="progress-bar" title="duration">
+                    <div id="progress" class="progress-bar progress-bar-striped" title="current time"></div>
+                </div>
+                <div class="text-monospace" id="time">
+                    <span id="currentTime">0:00</span>/<span id="fullime">0:00</span>
+                </div>
+            </div>
+            <div id="video-contact" class="fadeInUp">
+                <div class="card card-contact">
+                    <h5 class="card-header bg-gradient-mine text-white text-center text-uppercase">Contact Us</h5>
+                    <div class="card-body">
+                        <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+                        <form name="New Contact" id="__vtigerWebForm" action="https://websitetalkingheads.od1.vtiger.com/modules/Webforms/capture.php" enctype="multipart/form-data" method="post" accept-charset="utf-8">
+                            <input name="__vtrftk" type="hidden" value="sid:b79968d90e35e8ca67984873214e5597bbaee00c,1505250565">
+                            <input name="publicid" type="hidden" value="7d2174f0e1764a1915290b61d33c94b1">
+                            <input name="urlencodeenable" type="hidden" value="1">
+                            <input name="name" type="hidden" value="New Contact">
+                            <input name="__vtCurrency" type="hidden" value="1">
+                            <input name="designation" type="hidden" value="<?=$sentIP?>" data-label="">
+                            <input name="cf_1001" type="hidden" value="<?=$pageURL?>" data-label="">
+                            <table>
+                                <tbody>
+                                    <tr>
+                                        <td><label>Name*</label></td>
+                                        <td><input name="lastname" required="" type="text" value="" data-label=""></td>
+                                    </tr>
+                                    <tr>
+                                        <td><label>Email*</label></td>
+                                        <td><input name="email" required="" type="email" value="" data-label=""></td>
+                                    </tr>
+                                    <tr>
+                                        <td><label>Phone*</label></td>
+                                        <td><input name="phone" required="" type="text" value="" data-label=""></td>
+                                    </tr>
+                                    <tr>
+                                        <td><label>Message</label></td>
+                                        <td><textarea rows="3" name="description"></textarea></td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                            <div class="recaptcha-holder">
+                                <script src="https://www.google.com/recaptcha/api.js"></script>
+                                <div class="g-recaptcha" data-sitekey="6LcmdSATAAAAAGWw734vGo0AXQwuxJS7RmDZA_Fe"></div>
+                                <input id="captchaUrl" type="hidden" value="https://websitetalkingheads.od1.vtiger.com/modules/Settings/Webforms/actions/ValidateCaptcha.php">
+                                <input name="recaptcha_validation_value" id="recaptcha_validation_value" type="hidden">
+                            </div>
+                            <div class="text-center mt-2">
+                                <div class="submit-pointer">
+                                    <i class="fad fa-hand-point-right shake"></i>
+                                </div>
+                                <button id="vtigerFormSubmitBtn" type="submit" value="Submit" class="btn btn-custom center-block btn-lg text-center">SUBMIT</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="form-alt"></div>
+    <h3 class="text-center text-monospace" id="timeReporter"></h3>
+    <h1 id="reporter" class="text-center">Our Video Styles</h1>
+</section>
+<?php include("../../../includes/footer_b4.php"); ?>
+<script async src="talking-heads-player.js"></script>
+<script async  src="js/form.js"></script>
+<script>
+		$( document ).ready( function () {
+			createTalkingHead( "yes","mouse","#499FDE" );
+		} );
+	</script>
+</body>
+</html>
---
+++ talking-heads-player.js
@@ -0,0 +1,407 @@
+// JavaScript Document
+
+// Talking Heads Player version 0.9.1
+////controls- true,false, mouse
+//  autostart- no, yes, mute
+let talkingHeadsVideo = {
+    holder: $("#player-holder"),
+    player: $("#talking-head-player"),
+    container: {
+        controls: $("#controls")
+    },
+    btns: {
+        bigPlayBtn: $('#bigPlayBtn'),
+        restart: $('#btn-restart'),
+        playToggle: $('#btn-play-toggle'),
+        stop: $('#btn-stop'),
+        mute: $('#btn-mute'),
+        fullscreen: $('#btn-fullscreen')
+    },
+    started: false,
+    width: 1280,
+    height: 720
+};
+var th = talkingHeadsVideo.player,
+    title, curHotspot, hotspot, windowSize, newWidth;
+const progress = $("#progress"),
+    volumeBar = $("#volume-bar"),
+    holder = talkingHeadsVideo.holder,
+    btns = talkingHeadsVideo.btns,
+    progressBar = $("#progress-bar"),
+    time = $("#time"),
+    player = talkingHeadsVideo.player[0],
+    process = $("#elearning-process"),
+    chapterPath = "chapters/";
+
+function createTalkingHead(autostart, controls, color) {
+    //Hotspot creation
+    function loadChapter(currentChapter) {
+        let json = (function () {
+            let json = null;
+            $.ajax({
+                'async': false,
+                'global': false,
+                'url': chapterPath + currentChapter + ".json",
+                'dataType': "json",
+                'success': function (data) {
+                    json = data;
+                }
+            });
+            return json;
+        })();
+        title = json[0].video;
+        talkingHeadsVideo.chapter = json[0];
+        curHotspot = 0;
+        restartBar();
+    }
+    loadChapter("chapter1");
+    talkingHeadsVideo.portrait = window.matchMedia("(orientation: portrait)");
+    talkingHeadsVideo.landscape = window.matchMedia("(orientation: landscape)");
+    talkingHeadsVideo.showForm = false;
+    talkingHeadsVideo.showingForm = false;
+    talkingHeadsVideo.curWidth = $("#talking-head-player").width();
+    talkingHeadsVideo.curHeight = $("#talking-head-player").height();
+    talkingHeadsVideo.widthRatio = talkingHeadsVideo.curWidth / talkingHeadsVideo.width;
+    talkingHeadsVideo.autostart = autostart;
+    talkingHeadsVideo.controls = controls;
+    talkingHeadsVideo.path = "videos/";
+    talkingHeadsVideo.video = talkingHeadsVideo.path + title + ".mp4";
+    talkingHeadsVideo.poster = talkingHeadsVideo.path + "poster.jpg";
+    th.attr("src", talkingHeadsVideo.video);
+    $.get(talkingHeadsVideo.poster)
+        .done(function () {
+            th.attr("poster", talkingHeadsVideo.poster);
+        }).fail(function () {
+            console.log("fail");
+        });
+    setProgressBar();
+    checkOrientation();
+    //-------------------------------Set Color
+    process.css("background-color", "#" + color);
+    $("#controls").css("background-color", convertHex(color, 80));
+    $(".progress-bar").css("background-color", "#" + color);
+    $("#bigPlayBtn").css("background-color", convertHex(color, 70));
+    $("#bigPlayBtn").css("border-color", convertHex(color, 90));
+    //-------------------------------Set Controls
+    switch (talkingHeadsVideo.controls) {
+        case "true":
+            $("#controls").addClass("visible");
+            $("#controls").css("opacity", 1);
+            break;
+        case "false":
+            $("#controls").addClass("invisible");
+            break;
+        default:
+            talkingHeadsVideo.holder.addClass("mouse-controls");
+            break;
+    }
+    //--------------------------------Set autostart
+    tryAutostart();
+    //-----------------autostart poster
+    function poster() {
+        holder.click(function () {
+            talkingHeadsVideo.started = true;
+            holder.unbind();
+            player.load();
+            player.muted = false;
+            player.play();
+            btns.bigPlayBtn.hide("slow");
+            showPause();
+            btnFunctions();
+            time.text(showTime());
+        });
+    }
+    //Start functions--------------------------------------------------
+    function tryAutostart() {
+        let promise = player.play();
+        console.log(promise);
+        if (promise !== undefined) {
+            promise.then(_ => {
+                showPause();
+                btns.bigPlayBtn.hide("slow");
+                btnFunctions();
+            }).catch(error => {
+                poster();
+            });
+        }
+    }
+
+    function showPause() {
+        btns.playToggle.addClass("btn-pause");
+        btns.playToggle.removeClass("btn-play");
+        btns.bigPlayBtn.hide("slow");
+    }
+
+    function showPlay() {
+        btns.playToggle.removeClass("btn-pause");
+        btns.playToggle.addClass("btn-play");
+        btns.bigPlayBtn.show("slow");
+    }
+
+    function timedPause() {
+        btns.playToggle.removeClass("btn-pause");
+        btns.playToggle.addClass("btn-play");
+        player.pause();
+    }
+    //create button functions
+    function btnFunctions() {
+        //buttons functions--------------------------------------
+        $('#player-holder').click(function () {
+            if (!talkingHeadsVideo.started) {
+                talkingHeadsVideo.started = true;
+                player.currentTime = 0;
+                player.muted = false;
+                showPause();
+                th.removeAttr('loop');
+            } else {
+                switch (event.target.id) {
+                    case "btn-restart":
+                        restartBar();
+                        break;
+                    case "btn-play-toggle":
+                    case "bigPlayBtn":
+                        playToggle();
+                        break;
+                    case "btn-stop":
+                        stopPlayer();
+                        break;
+                    case "btn-mute":
+                        mutePlayer();
+                        break;
+                    case "progress":
+                    case "progress-bar":
+                        changeTime(event.offsetX);
+                        break;
+                    default:
+                        if (event.target.className === "hotspot") {
+                            if (talkingHeadsVideo.chapter.hotspots[curHotspot - 1].link === "none") {
+                                $("#player-holder").find(".hotspot").remove();
+                                playToggle();
+                            } else {
+                                var report = event.target.id;
+                                loadChapter(report);
+                                talkingHeadsVideo.video = talkingHeadsVideo.path + title + ".mp4";
+                                th.attr("src", talkingHeadsVideo.video);
+                                player.load();
+                                player.play();
+                                showPause();
+                                $("#player-holder").find(".hotspot").remove();
+                            }
+                        }
+                }
+            }
+        });
+    }
+
+    function playToggle() {
+        if (player.paused) {
+            btns.bigPlayBtn.hide("slow");
+            player.play();
+            showPause();
+        } else {
+            player.pause();
+            btns.bigPlayBtn.show("slow");
+            showPlay();
+        }
+    }
+
+    function stopPlayer() {
+        player.currentTime = 0;
+        player.pause();
+        showPlay();
+    }
+
+    function mutePlayer() {
+        if (player.muted) {
+            player.muted = false;
+            btns.mute.addClass("btn-unmute");
+            btns.mute.removeClass("btn-mute");
+        } else {
+            player.muted = true;
+            btns.mute.addClass("btn-mute");
+            btns.mute.removeClass("btn-unmute");
+        }
+    }
+
+    function changeTime(offset) {
+        let w = (offset / progressBar.width());
+        progress.css("width", w + '%');
+        player.pause();
+        player.currentTime = player.duration * w;
+        btns.bigPlayBtn.hide("slow");
+        player.play();
+    }
+    //video ended function
+    player.onended = function () {
+        player.currentTime = player.duration;
+    }
+    // Update the seek bar as the player plays
+    player.ontimeupdate = function () {
+        let progressBar = (player.currentTime / player.duration * 100);
+        progress.css("width", progressBar + "%");
+        time.text(showTime());
+        if (curHotspot < talkingHeadsVideo.chapter.hotspots.length) {
+            if (player.currentTime > talkingHeadsVideo.chapter.hotspots[curHotspot].time) {
+                if (talkingHeadsVideo.chapter.hotspots[curHotspot].pause) {
+                    timedPause();
+                }
+                if (talkingHeadsVideo.chapter.hotspots[curHotspot].link === "form") {
+                    if (talkingHeadsVideo.curWidth < 900 && !talkingHeadsVideo.showingForm) {
+                        talkingHeadsVideo.showForm = true;
+                        altForm();
+                    } else {
+                        $("#video-contact").css({
+                            "display": "inline"
+                        });
+                    }
+                } else {
+                    setHotspot(curHotspot);
+                    curHotspot++;
+                }
+            }
+        }
+    }
+    //Show time funcion
+    function showTime() {
+        let cur = getTime(player.currentTime);
+        let dur = getTime(player.duration);
+        $("#timeReporter").text(player.currentTime)
+        return cur + "/" + dur;
+    }
+    //get time - used for current time and duration
+    function getTime(timenow) {
+        if (parseInt(timenow) / 60 >= 1) {
+            let h = Math.floor(timenow / 3600);
+            if (isNaN(h)) {
+                h = 0
+            }
+            timenow = timenow - h * 3600;
+            let m = Math.floor(timenow / 60);
+            if (isNaN(m)) {
+                m = 0
+            }
+            let s = Math.floor(timenow % 60);
+            if (isNaN(s)) {
+                s = 0
+            }
+            if (m.toString().length < 2) {
+                m = m;
+            }
+            if (s.toString().length < 2) {
+                s = '0' + s;
+            }
+            return (m + ':' + s);
+        } else {
+            let m = Math.floor(timenow / 60);
+            if (isNaN(m)) {
+                m = 0
+            }
+            let s = Math.floor(timenow % 60);
+            if (isNaN(s)) {
+                s = 0
+            }
+            if (s.toString().length < 2) {
+                s = '0' + s;
+            }
+            return (m + ':' + s);
+        }
+    }
+    // Update the video volume
+    volumeBar.change(function () {
+        if (player.muted) {
+            player.muted = false;
+            btns.mute.addClass("btn-mute");
+            btns.mute.removeClass("btn-unmute");
+        }
+        player.volume = volumeBar.val();
+    });
+
+    function convertHex(hex, opacity) {
+        hex = hex.replace('#', '');
+        var r = parseInt(hex.substring(0, 2), 16);
+        var g = parseInt(hex.substring(2, 4), 16);
+        var b = parseInt(hex.substring(4, 6), 16);
+        var result = 'rgba(' + r + ',' + g + ',' + b + ',' + opacity / 100 + ')';
+        return result;
+    }
+
+    function restartBar() {
+        progress.css("transition", "none");
+        progress.css("width", "0%");
+        player.currentTime = 0;
+        progress.css("transition", "width .3s ease");
+    }
+
+    function altForm() {
+        talkingHeadsVideo.showingForm = true;
+        $("#form-alt").css("height", 440);
+        var playerForm = $("#video-contact");
+        playerForm.detach().appendTo("#form-alt");
+        $("#video-contact").css({
+            "display": "inline",
+            "position": "static",
+            "margin": "1rem auto"
+        });
+        $("#video-contact .card-contact").css({
+            "margin": "1rem auto"
+        });
+    }
+
+    function setHotspot(z) {
+        hotspot = $("#player-holder").append($('<div>', {
+            class: 'hotspot',
+            id: talkingHeadsVideo.chapter.hotspots[z].link
+        }).css({
+            "height": talkingHeadsVideo.chapter.hotspots[z].height + "%",
+            "width": talkingHeadsVideo.chapter.hotspots[z].width + "%",
+            "left": talkingHeadsVideo.chapter.hotspots[z].left + "%",
+            "top": talkingHeadsVideo.chapter.hotspots[z].top + "%",
+            "bottom": "auto",
+            "right": "auto"
+        }));
+    }
+    //Update controls on window resize
+    $(window).resize(function () {
+        setProgressBar();
+        checkOrientation();
+        if (talkingHeadsVideo.showForm && talkingHeadsVideo.curWidth < 900 && !talkingHeadsVideo.showingForm) {
+            altForm();
+        }
+    });
+    $(window).on("orientationchange", function () {
+        checkOrientation();
+    });
+
+    function checkOrientation() {
+        let heightTest = talkingHeadsVideo.curHeight - $(window).height();
+        if (talkingHeadsVideo.landscape.matches && heightTest > 0) {
+            process.height($(window).height());
+            process.width($(window).height() * 1.778);
+        } else {
+            process.css({
+                "width": "auto",
+                "height": "auto",
+            });
+        }
+    }
+//get controls width and set seekbar width
+function setProgressBar() {
+    talkingHeadsVideo.curWidth = $("#controls").outerWidth();
+    if (talkingHeadsVideo.curWidth < 500) {
+        volumeBar.css("display", "none");
+        volumeBar.width(0);
+    } else {
+        newWidth = parseInt(talkingHeadsVideo.curWidth / 8);
+        volumeBar.width(newWidth);
+        volumeBar.css("display", "block");
+    }
+    let width = 0;
+    $("#controls").children().each(function () {
+        let x = $(this).outerWidth(true);
+        width += x;
+    });
+    width = width - $("#progress-bar").outerWidth() + 10;
+    let progressBarWidth = (talkingHeadsVideo.curWidth - width) + "px";
+    progressBar.css("width", progressBarWidth);
+}
+}
